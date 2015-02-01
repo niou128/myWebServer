042b927f09475a0593d80f440efab615d9a06d6c
niou128 <niou128@gmail.com>
2015-02-01 15:18:24 +0100

    maj

---
+++ src/Observer.cpp
@@ -0,0 +1,1 @@
+#include "../headers/Observable.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,5 @@
 #include            "../headers/main.h"
 
-void* tata(void* data);
-void* tata(void* data)
-{
-  Server* ss = Server::getInstance();
-  string message = "";
-
-  while(1){
-    message = ss->sReceive();
-    ss->sendMessage(message);
-  }
-
-  if (data == 0){}   
-
-  return NULL;
-}
-
 int             main()
 {
 
